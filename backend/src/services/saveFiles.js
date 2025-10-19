a9aa7188738f33b3ed511b5402bf24ee943d2bc3
Tushar Langhnoda <tusharlanghnoda@gmail.com>
2025-10-18 23:08:05 -0600

    storing keys directly instead of presigned urls

--- backend/src/services/saveFiles.js
+++ backend/src/services/saveFiles.js
@@ -1,69 +1,34 @@
-import {
-  GetObjectCommand,
-  ListObjectsV2Command,
-  PutObjectCommand,
-} from "@aws-sdk/client-s3";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { s3 } from "../utils/aws/s3Client.js";
 import dotenv from "dotenv";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import supabase from "../utils/supabase/client.js";
 
 dotenv.config();
 
 async function uploadToS3(id, index, file) {
+  const key = `${id}-${index}-${file.originalname}`;
+
   const command = new PutObjectCommand({
     Bucket: process.env.S3_BUCKET_NAME,
-    Key: `${id}-${index}-${file.originalname}`,
+    Key: key,
     Body: file.buffer,
     ContentType: file.mimetype,
   });
 
   await s3.send(command);
-}
 
-async function getPresignedUrls(id, files) {
-  const command = new ListObjectsV2Command({
-    Bucket: process.env.S3_BUCKET_NAME,
-    Prefix: id,
-  });
-
-  const response = await s3.send(command);
-
-  const urls = [];
-
-  for (let i = 0; i < response.Contents.length; i++) {
-    const obj = response.Contents[i];
-
-    const command = new GetObjectCommand({
-      Bucket: process.env.S3_BUCKET_NAME,
-      Key: obj.Key,
-    });
-
-    const file = await getSignedUrl(s3, command, {
-      expiresIn: 60 * 60 * 24 * 4, //4 days
-    });
-
-    urls.push({
-      file_name: files[i].originalname,
-      file_type: files[i].mimetype,
-      key: obj.Key,
-      presignedUrl: file,
-      date: new Date().toISOString(),
-    });
-  }
-
-  return urls;
+  return { key: key, file_name: file.originalname, file_type: file.mimetype };
 }
 
 export async function saveFiles(id, files) {
-  await Promise.all(files.map((file, i) => uploadToS3(id, i, file)));
-
-  const urls = await getPresignedUrls(id, files);
+  const keys = await Promise.all(
+    files.map((file, i) => uploadToS3(id, i, file))
+  );
 
   const { data, error } = await supabase.from("files").insert([
     {
       parse_id: id,
-      files: urls,
+      files: keys,
     },
   ]);
 
