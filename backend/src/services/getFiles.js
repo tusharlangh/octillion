5d40c322e9cadf0da06ba876323e49908ddc2a24
Tushar Langhnoda <tusharlanghnoda@gmail.com>
2025-10-18 23:08:05 -0600

    storing keys directly instead of presigned urls

--- backend/src/services/getFiles.js
+++ backend/src/services/getFiles.js
@@ -1,4 +1,9 @@
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import supabase from "../utils/supabase/client.js";
+import dotenv from "dotenv";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { s3 } from "../utils/aws/s3Client.js";
+dotenv.config();
 
 export async function getFiles(id) {
   const { data, error } = await supabase
@@ -11,7 +16,29 @@ export async function getFiles(id) {
     return;
   }
 
-  const links = data.map((row) => row.files);
+  let links = data.map((row) => row.files);
+  links = links[0];
 
-  return links[0];
+  const urls = [];
+
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+
+    const command = new GetObjectCommand({
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: link.key,
+    });
+
+    const url = await getSignedUrl(s3, command, {
+      expiresIn: 60 * 60 * 24 * 1, //1 day
+    });
+
+    urls.push({
+      file_name: link.file_name,
+      file_type: link.mimetype,
+      presignedUrl: url,
+    });
+  }
+
+  return urls;
 }
