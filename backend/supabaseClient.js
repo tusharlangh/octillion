b41382ae4b12482cf25b9e8c5d37573f749d6073
Tushar Langhnoda <tusharlanghnoda@gmail.com>
2025-11-25 13:55:56 -0700

    set up basic logs

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
